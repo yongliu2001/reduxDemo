5276ab7079ffe707dae7d1351947dad56d943a08
Yong Liu <yong.liu2001@gmail.com>
2016-07-10 12:33:33 +1000

    react state and props example

---
+++ src/js/components/Header/Title.js
@@ -0,0 +1,15 @@
+/**
+ * Created by yongliu on 10/07/2016.
+ */
+
+import React from 'react';
+
+export default class Title extends React.Component {
+        
+    render() {
+        return (
+            <h1>{this.props.titleName}</h1>
+
+    )
+    }
+}
